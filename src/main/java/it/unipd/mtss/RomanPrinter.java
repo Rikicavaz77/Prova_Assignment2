a1915b2ed431bd32d9d4957b190afcf7c748b5c2
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2023-05-08 20:37:43 +0200

    Inizializzate classi base del progetto
    close #4

---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// Riccardo Cavalli 2042893
+// Pietro Giovanni Marchiorato 2042365
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class RomanPrinter {
+  public static String print(int num){
+  return printAsciiArt(IntegerToRoman.convert(num));
+  }
+  private static String printAsciiArt(String romanNumber){
+  return null;
+  }
+}
