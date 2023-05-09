3276b031b285e47dad60f5e06d2a2726c08092be
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2023-05-09 15:45:45 +0200

    modifica Test

--- src/test/java/it/unipd/mtss/business/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/business/IntegerToRomanTest.java
@@ -31,7 +31,7 @@ public class IntegerToRomanTest {
     @Test
     public void testConvert_WithMoreThan3999AsInput() throws RomanNumberException {
         thrown.expect(RomanNumberException.class);
-		thrown.expectMessage("Con i simboli I, V, X, L, C, D e M si possono rappresentare solo i numeri da 1 a 3999; per i numeri più grandi sono necessarie una o più linee sopra il simbolo");
+		thrown.expectMessage("Con i simboli I, V, X, L, C, D e M si possono rappresentare solo i numeri da 1 a 3999");
 
         IntegerToRoman.convert(4000);
     }
