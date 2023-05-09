094b494d5eacc891add221299b58be289723db40
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2023-05-09 15:42:39 +0200

    modifica IntToRoman per checkstyle

--- src/main/java/it/unipd/mtss/business/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/business/IntegerToRoman.java
@@ -10,11 +10,18 @@ import it.unipd.mtss.business.exceptions.RomanNumberException;
 public class IntegerToRoman 
 {
   public static String convert(int number) throws RomanNumberException {
-    if (number < 0) throw new RomanNumberException("I numeri negativi non sono convertibili in numero romano");
-    if (number == 0) throw new RomanNumberException("Lo 0 non è convertibile in numero romano");
-    if (number >= 4000) throw new RomanNumberException("Con i simboli I, V, X, L, C, D e M si possono rappresentare solo i numeri da 1 a 3999; per i numeri più grandi sono necessarie una o più linee sopra il simbolo");
-
-
+    if (number < 0) 
+    {
+      throw new RomanNumberException("I numeri negativi non sono convertibili in numero romano");
+    }
+    if (number == 0) 
+    {
+      throw new RomanNumberException("Lo 0 non è convertibile in numero romano");
+    }
+    if (number >= 4000) 
+    {
+      throw new RomanNumberException("Con i simboli I, V, X, L, C, D e M si possono rappresentare solo i numeri da 1 a 3999");
+    }
     return null;
   }
 }
