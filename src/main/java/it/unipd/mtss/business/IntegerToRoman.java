229c78874188607f517a1acc2d7d1c3497855601
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2023-05-11 20:03:50 +0200

    Conversione primi 500 numeri interi modifica

--- src/main/java/it/unipd/mtss/business/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/business/IntegerToRoman.java
@@ -14,8 +14,12 @@ public class IntegerToRoman
 
   public IntegerToRoman()
   {
-    ValuesRange = new int[]{500,400,100,90,50,40,10,9,5,4,1};  
-    RomanLetters = new String[]{"D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    ValuesRange = new int[]{
+      500,400,100,90,50,40,10,9,5,4,1
+    };  
+    RomanLetters = new String[]{
+      "D","CD","C","XC","L","XL","X","IX","V","IV","I"
+    };
   }
 
   public static String convert(int number) throws RomanNumberException {
